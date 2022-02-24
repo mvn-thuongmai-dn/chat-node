16319f35e48450b62252dec43d9ecab05ccca857
Thuong Mai X. VN.Danang <thuong.mai@monstar-lab.com>
2022-02-24 09:03:43 +0700

    test git final 4

--- test.js
+++ test.js
@@ -2,3 +2,4 @@ console.log(`test account github - not working`)
 console.log(`test account github - again`)
 console.log(`test account github - again 2`) // work
 console.log(`test account github - final test`)
+console.log(`test account github - final test 1`)
